787ed9a33737c5b9427968007d979b346f62d3e0
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2021-10-21 13:15:20 +0100

    Grill and Texture Changes
    - Updated Grill capabilities to be ready for changing textures on lit or not
    - Updated Grill texture
    - Updated Metal Pan and Metal Pot textures
    - Make the grill be hot on the top (Deals damage when stepped on) (This should be changed to only deal damage when it's lit)

--- src/main/java/net/zestyblaze/buffet/block/BrickGrillBlock.java
+++ src/main/java/net/zestyblaze/buffet/block/BrickGrillBlock.java
@@ -2,17 +2,29 @@ package net.zestyblaze.buffet.block;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
+import net.minecraft.block.entity.FurnaceBlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class BrickGrillBlock extends HorizontalFacingBlock {
+
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+    public static final BooleanProperty LIT = Properties.LIT;
 
     public static final BrickGrillBlock BRICK_GRILL = new BrickGrillBlock(FabricBlockSettings.of(
                     Material.REPAIR_STATION)
@@ -23,16 +35,26 @@ public class BrickGrillBlock extends HorizontalFacingBlock {
 
     protected BrickGrillBlock(Settings settings) {
         super(settings);
-        setDefaultState(this.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+        setDefaultState(this.getDefaultState().with(FACING, Direction.NORTH).with(LIT, false));
     }
 
     @Override
     protected void appendProperties(StateManager.@NotNull Builder<Block, BlockState> stateManager) {
-        stateManager.add(Properties.HORIZONTAL_FACING);
+        stateManager.add(FACING, LIT);
     }
 
     @Override
     public BlockState getPlacementState(@NotNull ItemPlacementContext ctx) {
         return this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
     }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, @NotNull Entity entity) {
+        if(!entity.isFireImmune() && entity instanceof LivingEntity) {
+            entity.damage(DamageSource.HOT_FLOOR, 1.0F);
+        }
+    }
+
+
 }
+
